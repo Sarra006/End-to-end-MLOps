18ef47be982e40930bc967a5835dcdf4414aea4c
EL FAIEZ Sarra <elfaiez.sarra@sesame.com.tn>
2025-05-22 15:07:58 +0100

    `Update Dockerfile to expose port 8501 and modify app.py to print additional message and run Streamlit with custom server address and port.`

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ WORKDIR /app
 COPY . /app
 RUN pip install -r requirements.txt
 
-EXPOSE 5000
+EXPOSE 8501
 
 CMD ["streamlit", "run", "app.py"]
--- app.py
+++ app.py
@@ -274,6 +274,7 @@ if __name__ == '__main__':
         print("✅ Entraînement réussi !")
         with open("training_done.flag", "w") as f:
             f.write("done")
+    print("✅ Chargement de l'application Streamlit...")
     os.system("streamlit run app.py --server.address=0.0.0.0 --server.port=5000")
     main()
     
